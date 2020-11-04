56705c60b512fce680141beef58818c49acb5f01
Dandy Satrio <dandysatrio123@gmail.com>
2020-11-04 22:12:40 +0700

    konfigurasi awal database, migrasi tabel, persiapan kodingan pelanggan

---
+++ app/Http/Controllers/pelanggan/indexController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\pelanggan;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class indexController extends Controller
+{
+    //
+}
---
+++ database/migrations/2020_11_04_103334_create_data_produk_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDataProdukTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data_produk', function (Blueprint $table) {
+            $table->increments('id_produk');
+            $table->string('nama_produk, 30');
+            $table->binary('foto_produk');
+            $table->integer('harga_produk');
+            $table->integer('stok');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data_produk');
+    }
+}
